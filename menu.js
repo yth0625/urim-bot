b1ea8a8964487dc3035462c55f59f8586815db94
yth0625 <yth0625@gmail.com>
2017-12-26 10:26:13 +0900

    Write menu.js

---
+++ menu.js
@@ -0,0 +1,37 @@
+const menu = [
+    [
+        [
+            "박가네 부대찌개", "순대국", "하코야", "미스터 스시", "추어탕", "생돼지 김치찌개", "포메인", "북창동 순두부찌개"
+        ],
+        [
+            "http://seotaean.com/wp-content/uploads/2014/03/IMAG3434.jpg", "http://mblogthumb3.phinf.naver.net/MjAxNjExMjhfMzIg/MDAxNDgwMzIwMjE1MDA2._YO-B8nuMS9XQkgT119aSmE06iXJJVtEO7KD0aI05acg.Z_XJHQDo3XsV1daWBPUaFlLst8SU6wBw7ri6di0bY-Ug.JPEG.hm3548/%EC%B9%A0%EB%B3%B4%EC%88%9C%EB%8C%80%EA%B5%AD-12.JPG?type=w800", "http://blogfiles5.naver.net/20150611_76/skfkddk1_1434031414295yMo1Q_JPEG/NaverBlog_20150611_230333_00.jpg", "http://cfile22.uf.tistory.com/image/256B5C415252026E355138", "http://m.논두렁추어탕.kr/data/admin/html_upload/images/304951cf42e863e3f832adf87cb1f938.jpg", "http://cfile26.uf.tistory.com/image/157E3C474F64662A1ACA86", "http://mblogthumb2.phinf.naver.net/20140804_149/44kgram_14071385930775ILLI_JPEG/20140328_%2827%29.jpg?type=w2", "https://ggtour.or.kr/wp-content/uploads/2014/02/781_01_800-300x225.jpg"
+        ]
+    ],
+    [
+        [
+            "KFC", "돈치킨", "우림 분식", "이화수", "새벽집"
+        ],
+        [
+            "https://www.onlyitaewon.com/wp-content/uploads/2016/08/KFC-Itaewon-2.jpg", "https://scontent-sea1-1.cdninstagram.com/t51.2885-15/s480x480/e35/13687224_569975323211204_799118534_n.jpg?ig_cache_key=MTMzMDIwODgwOTkxMzg4MTM4MA%3D%3D.2", "http://cfile235.uf.daum.net/image/21605E4C52C257ED3ADE90", "http://mblogthumb3.phinf.naver.net/MjAxNzA3MTZfODkg/MDAxNTAwMjEzNTY0MTMz.lJjrHLlDLwXLvNsr-jsUIvFcLKSJ8TdFfLCXfffggq0g.hEvpn5kq34b2tMggR4n6o5yi_KKolKGRYV31zBoIWU4g.JPEG.bkbros/%EC%9D%B4%ED%99%94%EC%88%98%EC%9C%A1%EA%B0%9C%EC%9E%A517.jpg?type=w800",
+            "http://mblogthumb3.phinf.naver.net/20151027_290/darcomm_1445926066220HjRcx_JPEG/IMG_5219.JPG?type=w2"
+        ]
+    ],
+    [
+        [
+            "백리향 중화요리", "리더 나인스", "순남 시래기", "큰집"
+        ],
+        [
+            "http://mblogthumb2.phinf.naver.net/MjAxNzAzMDNfOTAg/MDAxNDg4NTMyNjgxNzYy.crTx77W6doDUgvX6x-sOp8G_z10eDXMndsl7fsgQ3HYg.EBFIBxmoQitsW3pXloNmO7DUM3jfCVrrNfCP-CwhFeAg.JPEG.adventure_kr/image_5865115561488532581335.jpg?type=w800", "http://cfile236.uf.daum.net/image/2279DC3A569F0BB93DBF8F", "http://cfile225.uf.daum.net/image/275A73345930F0FC2DA0EE", "http://cfile203.uf.daum.net/image/256F453351FDC5D0291138"
+        ]
+    ],
+    [
+        [
+            "맥도날드", "이마트"
+        ],
+        [
+            "http://cfile23.uf.tistory.com/image/995BF83359D9C3DB1EECA5", "http://cfile28.uf.tistory.com/image/242E5F3F5688FABD2D4A47"
+        ]
+    ]
+];
+
+module.exports = menu;
