5b1a87dd66949371c6348c194c8185eb4d4710d0
Taehyeon Yun <yth0625@gmail.com>
2019-05-13 16:28:28 +0900

    refactorying app.js

--- app.js
+++ app.js
@@ -1,67 +1,48 @@
-import express from "express";
-import bodyparser from "body-parser";
-import menu from "./menu.js";
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const menus = require('./menu');
+
+const SERVER_URL = process.env['SERVER_URL'];
+const PORT = process.env['PORT'] || 8625;
+
 const app = express();
-const server_url = '';
-const port = 8625;
 app.use(bodyparser.urlencoded({'extended':'true'}));
 app.use(bodyparser.json());
-
-function action(name, floor) {
-    return {
-        name: name,
-        integration: {
-            url: `${server_url}:${port}/choice`,
-            context: {
-                floor: floor
-            }
-        }
-    };
-}
-
+  
 app.post("/urim", (req, res) => {
     const attachments = [{
-        "text": "점심먹을 장소를 골라주세요.",
-        "actions": [
-            action("3층에서 먹기", 0), 
-            action("1층에서 먹기", 1), 
-            action("지하에서 먹기", 2),
-            action("밖에서 먹기", 3),
-            action("아무거나 먹기", 4)
-        ]
+        text: '점심먹을 장소를 골라주세요.',
+        actions: ['3층', '1층', '지하', '바깥', null].map(location => ({
+            name: location ? `${location}에서 먹기` : '아무데서나 먹기',
+            integration: {
+                url: `${SERVER_URL}:${PORT}/choice`,
+                context: { location }
+            }
+        }))
     }];
-    res.send({
-        response_type: "in_channel", attachments: attachments
-    });
+
+    res.send({ response_type: 'in_channel', attachments });
 });
 
 app.post("/choice", (req, res) => {
-    const choice_value = req.body.context.floor;
-    let attachments = [{
-        title: "오늘의 점심",
-        fields: [
-            {
-                short: false,
-                value: ""
-            }],
-        image_url: ""
+    const { location } = req.body.context;
+    const range = location
+        ? menus.filter(menu => menu.location === location)
+        : menus;
+
+    const selected = range[~~(Math.random() * range.length)];
+
+    const attachments = [{
+        title: '오늘의 점심',
+        fields: [{
+            short: false,
+            value: `오늘의 점심은 ${selected.name}에서 먹습니다!`
+        }],
+        image_url: selected.picUrl
     }];
-    if (choice_value === 4) {
-        const random_floor = Math.floor(Math.random() * menu.length);
-        const random_food = Math.floor(Math.random() * menu[random_floor][0].length);
-        attachments[0].fields[0].value = "오늘의 점심은 " + menu[random_floor][0][random_food] + "에서 먹습니다!";
-        attachments[0].image_url = menu[random_floor][1][random_food];
-    } else {
-        const random_food = Math.floor(Math.random() * menu[choice_value][0].length);
-        attachments[0].fields[0].value = "오늘의 점심은 " + menu[choice_value][0][random_food] +  "에서 먹습니다!";
-        attachments[0].image_url = menu[choice_value][1][random_food];
-    }
-    res.send({
-        update: {
-            props: {
-                attachments: attachments
-            }
-        }
-    });
+
+    res.send({ update: { props: { attachments } } });
 });
-app.listen(port, () => console.log('app listening on port ' + port));
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
